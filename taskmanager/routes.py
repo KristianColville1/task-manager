19e7a2822dd03a55ed9f5edceb8630cb20926182
KristianColville1 <kristiancolville1@gmail.com>
2022-06-26 11:53:06 +0100

    Add routes for tasks and categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -6,3 +6,11 @@ from taskmanager.models import Category, Task
 @app.route("/")
 def home():
     return render_template("base.html")
+
+@app.route("/tasks")
+def tasks():
+    return render_template("tasks.html")
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
