6f3f89fcea6b0288a8adeb2449d48708c3e61759
KristianColville1 <kristiancolville1@gmail.com>
2022-06-29 07:31:03 +0100

    Added tasks functionality to view current tasks

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,3 @@
-from enum import unique
 from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
@@ -6,7 +5,8 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    return render_template("tasks.html")
+    tasks = list(Task.query.order_by(Task.id).all())
+    return render_template("tasks.html", tasks=tasks)
 
 
 @app.route("/categories")
